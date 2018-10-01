ec9ff4e73cdff6f2500cd624990c0c7f0839b761
Rohit Kumar <rohit9075@gmail.com>
2018-10-01 16:29:15 +0530

    java and gradle updates

--- app/src/main/java/com/rohit/snackbar/MainActivity.java
+++ app/src/main/java/com/rohit/snackbar/MainActivity.java
@@ -1,8 +1,8 @@
 package com.rohit.snackbar;
 
+import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
